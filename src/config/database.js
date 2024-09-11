33829ecf8a2cec59b5fb291a464d519f5a218555
JoaoPedroTrindade21 <joaotrindade.work@gmail.com>
2024-09-11 14:46:13 -0300

    devburguer-api

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dialect : 'postgres',
+    url: 'postgresql://postgres:ZrzGhPRpaUevdLSLVFUdGeKkjdQyHhbl@autorack.proxy.rlwy.net:29582/railway',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
